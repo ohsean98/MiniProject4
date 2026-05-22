1da0188cc4e6f3c9a78cbeb0b01b6535a897ff9e
HyeonWoo Cho <robert12154937@gmail.com>
2026-05-22 13:40:30 +0900

    Delete walking-library directory

--- walking-library/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <>
-      <h1 style={{ color: "#333", marginBottom: "10px" }}>📚 걷기가 서재 : 작가의 산책</h1>
-      <hr style={{ border: "1px solid #ddd", marginBottom: "20px" }} />
-    </>
-  );
-}
